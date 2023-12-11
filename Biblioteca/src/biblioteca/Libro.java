63668020041d990941d73b3511f1697c9c149ba5
PlansVsAngry999 <138540802+PlansVsAngry999@users.noreply.github.com>
2023-12-11 08:35:38 -0600

    Subir proyecto...

---
+++ Biblioteca/src/biblioteca/Libro.java
@@ -0,0 +1,38 @@
+package biblioteca;
+/**
+ *
+ * @author Ricardo Daniel Hernández Sosa (PlansVsAngry999).
+ */
+public class Libro {
+    private String titulo;
+    private int id;
+    private String autor;
+        //
+    public Libro(String titulo, String autor, int id){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.id = id;
+    }
+        //
+    public String getTitulo(){
+        return titulo;
+    }
+        //
+    public String getAutor(){
+        return autor;
+    }
+        //
+    public int getId(){
+        return id;
+    }
+    
+    @Override
+    public String toString(){
+        return "Libro( "+
+                "Titulo = '" + titulo + '\'' +
+                ", Autor = '" + autor + '\'' +
+                ",id = '" + id +
+                '}';
+    }
+}
+
---
+++ Biblioteca/src/biblioteca/Main.java
@@ -0,0 +1,27 @@
+package biblioteca;
+/**
+ *
+ * @author Ricardo Daniel Hernández Sosa (PlansVsAngry999).
+ */
+public class Main {
+    public static void main(String[] args){
+    Biblioteca biblioteca = new Biblioteca();
+        //Crear y agregar libros.
+    Libro libro1 = new Libro("El señor de los anillos", "J.R.R Tolkien", 1);
+    Libro libro2 = new Libro("Cien años de soledad", "Gabriel Garcia Marquez", 2);
+    biblioteca.agregarLibro(libro1);
+    biblioteca.agregarLibro(libro2);
+        //Crear y registrar usuarios.
+        Usuario user1 = new Usuario("Juan", 101);
+        Usuario user2 = new Usuario("Mario", 202);
+        biblioteca.registrarUsuario(user1);
+        biblioteca.registrarUsuario(user2);
+            //Prestar y devolver libros.
+            user1.prestarLibro(libro1);
+            user2.prestarLibro(libro2);
+            user1.devolverLibro(libro1);
+                //Mostrar información de la biblioteca.
+                biblioteca.mostrarCatalogo();
+                biblioteca.mostrarUsuario();
+    }
+}
